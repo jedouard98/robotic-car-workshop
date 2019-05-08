00e156875e5d473a3e8e4d53f3ed3a5e2696de46
jedouard98 <jedouard98@gmail.com>
2019-05-08 12:42:44 -0400

    Update makers_car_workshop/makers_car_workshop.ino

--- makers_car_workshop/makers_car_workshop.ino
+++ makers_car_workshop/makers_car_workshop.ino
@@ -1,61 +1,58 @@
 /*  Arduino DC Motor Control - PWM | H-Bridge | L298N
          Example 02 - Arduino Robot Car Control
-    by Dejan Nedelkovski, www.HowToMechatronics.com
+   Credit to Dejan Nedelkovski, www.HowToMechatronics.com
 */
-#define enA 9
-#define in1 4
-#define in2 5
-#define enB 10
-#define in3 6
-#define in4 7
-
-int motorSpeedA = 0;
-int motorSpeedB = 0;
+const int enAPin = 6; // Left motor PWM speed control
+const int in1Pin = 7; // Left motor Direction 1
+const int in2Pin = 5; // Left motor Direction 2
+const int in3Pin = 4; // Right motor Direction 1
+const int in4Pin = 2; // Right motor Direction 2
+const int enBPin = 3; // Right motor PWM speed control
 
 void setup() {
-  pinMode(enA, OUTPUT);
-  pinMode(enB, OUTPUT);
-  pinMode(in1, OUTPUT);
-  pinMode(in2, OUTPUT);
+  pinMode(enAPin, OUTPUT);
+  pinMode(enBPin, OUTPUT);
+  pinMode(in1Pin, OUTPUT);
+  pinMode(in2Pin, OUTPUT);
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
 }
 
 void moveForward() {
     // Set Motor A forward
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, HIGH);
+    digitalWrite(in1Pin, LOW);
+    digitalWrite(in2Pin, HIGH);
     // Set Motor B forward
-    digitalWrite(in3, LOW);
-    digitalWrite(in4, HIGH);
+    digitalWrite(in3Pin, LOW);
+    digitalWrite(in4Pin, HIGH);
 
     // Speeds for motors Must be between 0 and 255
     int motorSpeedA = 200;
     int motorSpeedB = 200;
-    analogWrite(enA, motorSpeedA); // Send PWM signal to motor A
-    analogWrite(enB, motorSpeedB); // Send PWM signal to motor B
+    analogWrite(enAPin, motorSpeedA); // Send PWM signal to motor A
+    analogWrite(enBPin, motorSpeedB); // Send PWM signal to motor B
 }
 
 void moveBackwards() {
      // Set Motor A backward
-    digitalWrite(in1, HIGH);
-    digitalWrite(in2, LOW);
+    digitalWrite(in1Pin, HIGH);
+    digitalWrite(in2Pin, LOW);
     // Set Motor B backward
-    digitalWrite(in3, HIGH);
-    digitalWrite(in4, LOW);
+    digitalWrite(in3Pin, HIGH);
+    digitalWrite(in4Pin, LOW);
 
     // Speeds for motors Must be between 0 and 255
     int motorSpeedA = 200;
     int motorSpeedB = 200;
-    analogWrite(enA, motorSpeedA); // Send PWM signal to motor A
-    analogWrite(enB, motorSpeedB); // Send PWM signal to motor B
+    analogWrite(enAPin, motorSpeedA); // Send PWM signal to motor A
+    analogWrite(enBPin, motorSpeedB); // Send PWM signal to motor B
 }
 
 void stopMoving() {
     int motorSpeedA = 0;
     int motorSpeedB = 0;
-    analogWrite(enA, motorSpeedA); // Send PWM signal to motor A
-    analogWrite(enB, motorSpeedB); // Send PWM signal to motor B
+    analogWrite(enAPin, motorSpeedA); // Send PWM signal to motor A
+    analogWrite(enBPin, motorSpeedB); // Send PWM signal to motor B
 }
 
 void moveLeft() {
